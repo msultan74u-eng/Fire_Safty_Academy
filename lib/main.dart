c89999d7a84df02df0df210ef6864ef70cfb4364
Sultan <msultan.74u@gmail.com>
2026-03-13 02:50:37 +0200

    usage main splash , onboarding and themes in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
-import 'package:fire_safty_academy/views/home_page.dart';
+import 'package:fire_safty_academy/views/splach_screen/main_splash.dart';
 import 'package:flutter/material.dart';
+
+import 'core/themes/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+  State<MyApp> createState() => _MyAppState();
+}
 
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home:  HomePage(),
+class _MyAppState extends State<MyApp> {
+  // نستخدم ValueNotifier
+  ValueNotifier<bool> isDark = ValueNotifier(true);
+
+  void toggleTheme() {
+    isDark.value = !isDark.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: isDark,
+      builder: (context, value, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: value ? ThemeMode.dark : ThemeMode.light,
+          title: 'Fire Safety Academy',
+          home: MainSplash(toggleTheme: toggleTheme, isDarkNotifier: isDark),
+        );
+      },
     );
   }
 }
-
-
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,9 +1,16 @@
-
-
 import 'package:flutter/material.dart';
 
+import '../core/themes/app_colors.dart';
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({
+    super.key,
+    required this.isDarkNotifier,
+    required this.toggleTheme,
+  });
+
+  final ValueNotifier<bool> isDarkNotifier;
+  final VoidCallback toggleTheme;
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -13,16 +20,56 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 120,
-          decoration: BoxDecoration(
-            color: Colors.indigo
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        title: Text(
+          'Fire Safty Academy',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
           ),
-
         ),
+        centerTitle: true,
+
+        actions: [
+          ValueListenableBuilder<bool>(
+            valueListenable: widget.isDarkNotifier,
+            builder: (context, isDark, child) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Ink(
+                  width: 36, // حجم الدائرة صغير
+                  height: 36, // حجم الدائرة صغير
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(
+                      0.2,
+                    ), // شفاف جزئي مناسب على الأحمر
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.15),
+                        blurRadius: 3,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: IconButton(
+                    iconSize: 20, // حجم الأيقونة أصغر ليتناسب مع الدائرة
+                    icon: Icon(
+                      isDark ? Icons.light_mode : Icons.dark_mode,
+                      color: Colors.white,
+                    ),
+                    onPressed: widget.toggleTheme,
+                    tooltip: 'Toggle Theme',
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
+      body: Container(color: Theme.of(context).scaffoldBackgroundColor),
     );
   }
 }
