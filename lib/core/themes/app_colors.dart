41a47fafaf2040f91529a55904bd46b64424b046
Sultan <msultan.74u@gmail.com>
2026-03-13 02:48:35 +0200

    create color app file

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFD32F2F);
+  static const secondary = Color(0xFFFF6F00);
+
+  static const background = Color(0xFFF5F5F5);
+  static const card = Colors.white;
+
+  static const textLight = Color(0xFF000000);
+  static const  textDark = Color(0xFFFFFFFF);
+}
