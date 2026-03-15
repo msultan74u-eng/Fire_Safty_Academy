d855a3200207d56bf607e57e596a95c9196346ac
Sultan <msultan.74u@gmail.com>
2026-03-15 22:15:41 +0200

    create FireGridHome widget

---
+++ lib/widgets/home_Widgets/fire_grid_home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'fire_card_home.dart';
+
+class FireGridHome extends StatefulWidget {
+  const FireGridHome({super.key});
+
+  @override
+  State<FireGridHome> createState() => _FireGridHomeState();
+}
+
+class _FireGridHomeState extends State<FireGridHome> {
+  final items = [
+    {
+      "title": "Fire Alarm",
+      "bg": "assets/images/main_design/fire_bg_001.jpg",
+      "icon": "assets/images/main_design/facp_0002.jpg",
+    },
+    {
+      "title": "Fire Fighting",
+      "bg": "assets/images/main_design/fire_bg_001.jpg",
+      "icon": "assets/images/main_design/facp_0002.jpg",
+    },
+    {
+      "title": "Detectors",
+      "bg": "assets/images/main_design/fire_bg_001.jpg",
+      "icon": "assets/images/main_design/facp_0002.jpg",
+    },
+    {
+      "title": "Extinguishers",
+      "bg": "assets/images/main_design/fire_bg_001.jpg",
+      "icon": "assets/images/main_design/facp_0002.jpg",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return FireCardHome(
+          title: item["title"]!,
+          backgroundImage: item["bg"]!,
+          iconImage: item["icon"]!,
+          onTap: () {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text('${item["title"]} clicked')));
+          },
+        );
+      },
+    );
+  }
+}
